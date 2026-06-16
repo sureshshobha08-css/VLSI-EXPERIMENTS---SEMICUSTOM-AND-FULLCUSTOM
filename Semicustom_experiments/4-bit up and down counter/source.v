528c6b134429401c9617bb809cdc080453288aa5
Shobha J S <sureshshobha54@gmail.com>
2026-06-16 07:56:55 +0530

    Create source.v

---
+++ Semicustom_experiments/4-bit up and down counter/source.v
@@ -0,0 +1,13 @@
+module counter(clk,m,rst,count);
+input clk,m,rst;
+output reg [3:0] count;
+always @(posedge clk or negedge rst)
+begin
+if (!rst)
+count = 0;
+else if(m)
+count = count+1;
+else
+count = count-1;
+end 
+endmodule
